9946747f9836ec965d5928491fcecefb87ce4174
YourtionGuo <yourtion@gmail.com>
2015-12-20 14:57:47 +0800

    init

---
+++ SDebug/SDebug/SDebug.h
@@ -0,0 +1,19 @@
+//
+//  SDebug.h
+//  SDebug
+//
+//  Created by YourtionGuo on 12/19/15.
+//  Copyright © 2015 Yourtion. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for SDebug.
+FOUNDATION_EXPORT double SDebugVersionNumber;
+
+//! Project version string for SDebug.
+FOUNDATION_EXPORT const unsigned char SDebugVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <SDebug/PublicHeader.h>
+
+
